a2793407be198f0ba0663a8835dace15886360e1
pavsoldatov <snowschoe@gmail.com>
2022-04-17 17:21:33 +0300

    Got UI done (mostly), reworked logic for certain functions

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const entities = ["rock", "paper", "scissors"];
 
+let buttons = document.querySelectorAll(".btn");
+let log = document.querySelector(".log");
+let para = document.createElement("p");
+
+log.appendChild(para);
+let resetBtn = document.createElement("button");
+
 let playerScore = 0;
 let computerScore = 0;
 
@@ -12,70 +19,65 @@ function computerPlay(array) {
   return array[randomIndex];
 }
 
-function playRound() {
-  let playerSelection = prompt("Your weapon of choice?").toLowerCase();
-  let computerSelection = computerPlay(entities);
+buttons.forEach((button) => {
+  button.addEventListener("click", playRound);
+});
 
-  console.log(`Player: ${playerSelection}`);
-  console.log(`Computer: ${computerSelection}`);
-
-  if (
-    !(
-      playerSelection === "rock" ||
-      playerSelection === "scissors" ||
-      playerSelection === "paper"
-    )
-  ) {
-    alert("Please enter a valid input: rock, paper or sccisors.");
-    return;
-  } else if (playerSelection === "rock" && computerSelection === "paper") {
-    ++computerScore;
-    alert(
-      `Computer won. Paper beats rock. \nComputer ${computerScore}, Player ${playerScore}`
-    );
-  } else if (playerSelection === "rock" && computerSelection === "scissors") {
-    ++playerScore;
-    alert(
-      `You won. Rock beats scissors. \nComputer ${computerScore}, Player ${playerScore}`
-    );
-  } else if (playerSelection === "paper" && computerSelection === "rock") {
-    ++playerScore;
-    alert(
-      `You won. Paper beats rock. \nComputer ${computerScore}, Player ${playerScore}`
-    );
-  } else if (playerSelection === "paper" && computerSelection === "scissors") {
-    ++computerScore;
-    alert(
-      `Computer won. Scissors beat paper. \nComputer ${computerScore}, Player ${playerScore}`
-    );
-  } else if (playerSelection === "scissors" && computerSelection === "rock") {
-    ++computerScore;
-    alert(
-      `Computer won. Rock beats scissors. \nComputer ${computerScore}, Player ${playerScore}`
-    );
-  } else if (playerSelection === "scissors" && computerSelection === "paper") {
-    ++playerScore;
-    alert(
-      `You won. Scissors beat paper. \nComputer ${computerScore}, Player ${playerScore}`
-    );
-  } else {
-    alert(
-      `It's a tie. Nobody wins. \nComputer ${computerScore}, Player ${playerScore}`
-    );
-  }
+function removeEventListener() {
+  buttons.forEach((button) => {
+    button.removeEventListener("click", playRound);
+  });
 }
 
-for (let i = 0; i < 500; i++) {
+function playRound() {
+  while (playerScore < 5 && computerScore < 5) {
+    let playerSelection = this.value;
+    let computerSelection = computerPlay(entities);
+
+    // Переделать сообщения в textContent
+    if (playerSelection === "rock" && computerSelection === "paper") {
+      ++computerScore;
+      para.textContent = `Computer won. Paper beats rock. \nComputer ${computerScore}, Player ${playerScore}`;
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+      ++playerScore;
+      para.textContent = `You won. Rock beats scissors. \nComputer ${computerScore}, Player ${playerScore}`;
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+      ++playerScore;
+      para.textContent = `You won. Paper beats rock. \nComputer ${computerScore}, Player ${playerScore}`;
+    } else if (
+      playerSelection === "paper" &&
+      computerSelection === "scissors"
+    ) {
+      ++computerScore;
+      para.textContent = `Computer won. Scissors beat paper. \nComputer ${computerScore}, Player ${playerScore}`;
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+      ++computerScore;
+      para.textContent = `Computer won. Rock beats scissors. \nComputer ${computerScore}, Player ${playerScore}`;
+    } else if (
+      playerSelection === "scissors" &&
+      computerSelection === "paper"
+    ) {
+      ++playerScore;
+      para.textContent = `You won. Scissors beat paper. \nComputer ${computerScore}, Player ${playerScore}`;
+    } else {
+      para.textContent = `It's a tie. Nobody wins. \nComputer ${computerScore}, Player ${playerScore}`;
+    }
+
+    console.log(`Player: ${playerSelection}`);
+    console.log(`Computer: ${computerSelection}`);
+    showScore();
+    return;
+  }
+  removeEventListener();
+}
+
+function showScore() {
   if (playerScore === 5) {
-    alert(`Player: ${playerScore}, Computer: ${computerScore}. You have won!`);
-    break;
+    para.innerText = `You have won! Final score:\nplayer: ${playerScore}, computer: ${computerScore}.`;
+    return;
   }
   if (computerScore === 5) {
-    alert(
-      `Player: ${playerScore}, Computer: ${computerScore}. Computer has won!`
-    );
-    break;
+    para.innerText = `Computer has won! Final score:\nplayer: ${playerScore}, computer: ${computerScore}.`;
+    return;
   }
-  playRound();
-  console.log(`Player: ${playerScore}, Computer: ${computerScore}`);
 }
