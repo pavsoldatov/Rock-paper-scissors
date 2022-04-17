bd68a6e91cc90000d03347152affeecce875edd5
pavsoldatov <snowschoe@gmail.com>
2022-04-17 18:42:51 +0300

    added a reset button, refactored the rps array into respective objects to include respective emojis

--- script.js
+++ script.js
@@ -1,77 +1,85 @@
-const entities = ["rock", "paper", "scissors"];
+let rock = { name: "rock", emoji: "👊" };
+let paper = { name: "paper", emoji: "🤚" };
+let scissors = { name: "scissors", emoji: "✌️" };
+const entities = [rock, paper, scissors];
+
 
 let buttons = document.querySelectorAll(".btn");
 let log = document.querySelector(".log");
-let para = document.createElement("p");
+let para = document.querySelector(".log > p");
 
 log.appendChild(para);
-let resetBtn = document.createElement("button");
 
 let playerScore = 0;
 let computerScore = 0;
 
+/* utility functions */
 function random(number) {
   return Math.floor(Math.random() * number);
 }
 
 function computerPlay(array) {
   let randomIndex = random(array.length);
-  return array[randomIndex];
+  return array[randomIndex].name; // remember to toy around object.name/emoji
 }
+/* utility functions */
 
 buttons.forEach((button) => {
   button.addEventListener("click", playRound);
 });
 
-function removeEventListener() {
+/* Removing listener upon reaching 5 victories during playRound */
+function removeListener() {
   buttons.forEach((button) => {
     button.removeEventListener("click", playRound);
   });
 }
+/* Removing listener upon reaching 5 victories during playRound */
 
 function playRound() {
   while (playerScore < 5 && computerScore < 5) {
     let playerSelection = this.value;
     let computerSelection = computerPlay(entities);
 
-    // Переделать сообщения в textContent
+    // I need to refactor the strings, so that they refer to the object's name
+    // and do not require hardcoding the string value (ie, rock)
     if (playerSelection === "rock" && computerSelection === "paper") {
       ++computerScore;
-      para.textContent = `Computer won. Paper beats rock. \nComputer ${computerScore}, Player ${playerScore}`;
+      para.innerText = `Computer${paper.emoji}  vs  Player${rock.emoji}. Paper beats rock. \nComputer: ${computerScore}, Player: ${playerScore}`;
     } else if (playerSelection === "rock" && computerSelection === "scissors") {
       ++playerScore;
-      para.textContent = `You won. Rock beats scissors. \nComputer ${computerScore}, Player ${playerScore}`;
+      para.innerText = `Computer${scissors.emoji} vs Player${rock.emoji}. Rock beats scissors. \nComputer ${computerScore}, Player ${playerScore}`;
     } else if (playerSelection === "paper" && computerSelection === "rock") {
       ++playerScore;
-      para.textContent = `You won. Paper beats rock. \nComputer ${computerScore}, Player ${playerScore}`;
+      para.tinnerText = `Computer${rock.emoji} vs Player${paper.emoji}. Paper beats rock. \nComputer ${computerScore}, Player ${playerScore}`;
     } else if (
       playerSelection === "paper" &&
       computerSelection === "scissors"
     ) {
       ++computerScore;
-      para.textContent = `Computer won. Scissors beat paper. \nComputer ${computerScore}, Player ${playerScore}`;
+      para.innerText = `Computer${scissors.emoji} vs Player${paper.emoji}. Scissors beat paper. \nComputer ${computerScore}, Player ${playerScore}`;
     } else if (playerSelection === "scissors" && computerSelection === "rock") {
       ++computerScore;
-      para.textContent = `Computer won. Rock beats scissors. \nComputer ${computerScore}, Player ${playerScore}`;
+      para.textContent = `Computer${rock.emoji} vs Player${scissors.emoji}. Rock beats scissors. \nComputer ${computerScore}, Player ${playerScore}`;
     } else if (
       playerSelection === "scissors" &&
       computerSelection === "paper"
     ) {
       ++playerScore;
-      para.textContent = `You won. Scissors beat paper. \nComputer ${computerScore}, Player ${playerScore}`;
+      para.textContent = `Computer${paper.emoji} vs Player${scissors.emoji}. Scissors beat paper. \nComputer ${computerScore}, Player ${playerScore}`;
     } else {
       para.textContent = `It's a tie. Nobody wins. \nComputer ${computerScore}, Player ${playerScore}`;
     }
 
     console.log(`Player: ${playerSelection}`);
     console.log(`Computer: ${computerSelection}`);
-    showScore();
+    showFinalScore();
     return;
   }
-  removeEventListener();
+  removeListener();
 }
 
-function showScore() {
+function showFinalScore() {
   if (playerScore === 5) {
     para.innerText = `You have won! Final score:\nplayer: ${playerScore}, computer: ${computerScore}.`;
     return;
@@ -81,3 +89,24 @@ function showScore() {
     return;
   }
 }
+
+/* Restarting the game */
+function restartGame() {
+  console.log(playerScore, computerScore);
+
+  playerScore = 0;
+  computerScore = 0;
+  console.log(playerScore, computerScore);
+
+  para.innerText = "";
+
+  buttons.forEach((button) => {
+    button.addEventListener("click", playRound);
+  });
+  para.innerText = "Press the button below!"
+}
+
+let restartBtn = document.querySelector(".restart-btn");
+restartBtn.addEventListener("click", restartGame);
+console.log(restartBtn);
+/* Restarting the game */
